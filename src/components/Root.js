9deae239851e7e782549c1128f89f4206a09820c
Usman Afzal <usmanafzal@usmans-air.mynet>
2019-08-15 00:12:30 +0100

    [done] exercise 7

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './Login/index'
 import App from './App'
 
 const Root = () => (
   <Router>
-    <Route path="/" component={App} />
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/" component={App} />
+    </Switch>
   </Router>
 )
 
